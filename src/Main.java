7419be210b532e1427f30b49d879e58591ed532e
ConnorValerio <connorvalerio@gmail.com>
2018-11-28 11:09:58 +0000

    Implementing Regex Simplexer
    Completed Regex Simplexer and completed a simple test against the
    Simplexer. Added gitignore.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,46 @@
 
 public class Main {
 	public static void main(String[] args) {
-		Simplexer s = new Simplexer("hello world!");
-		RegexSimplexer rs = new RegexSimplexer("hello world!");
+
+		/*
+		 * Test for loop at 1000000 iterations:
+		 * 
+		 * Simplexer took: 8215ms REGEX | Simplexer took: 22666ms.
+		 * 
+		 * Recompiling REGEX Patterns takes its toll.
+		 */
+
+		/* One simple test, not a test class - use JUnit Later */
+		final int TESTING_FACTOR = 1000000;
+
+		System.out.println("Testing Simplexer...");
+
+		long start1 = System.currentTimeMillis();
+		for (int i = 0; i < TESTING_FACTOR; i++) {
+			Simplexer s = new Simplexer("if (x = 5) y = x / 5 + 1 else if(x = false) y = 0 else return y");
+			Token t;
+			while (s.hasNext()) {
+				t = s.next();
+				// System.out.println("TOKEN: " + t.getText() + ", " + t.getType());
+			}
+		}
+		long end1 = System.currentTimeMillis();
+
+		System.out.println("Testing REGEX Simplexer...\n");
+
+		long start2 = System.currentTimeMillis();
+		for (int i = 0; i < TESTING_FACTOR; i++) {
+			RegexSimplexer rs = new RegexSimplexer("if (x = 5) y = x / 5 + 1 else if(x = false) y = 0 else return y");
+			Token t;
+			while (rs.hasNext()) {
+				t = rs.next();
+				// System.out.println("TOKEN: " + t.getText() + ", " + t.getType());
+			}
+		}
+		long end2 = System.currentTimeMillis();
+
+		System.out.println("\nSimplexer took: " + (end1 - start1) + "ms.");
+		System.out.println("REGEX Simplexer took: " + (end2 - start2) + "ms.");
+
 	}
 }
--- src/RegexSimplexer.java
+++ src/RegexSimplexer.java
@@ -1,10 +1,64 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class RegexSimplexer {
+public class RegexSimplexer implements Iterator<Token> {
+
+	/*
+	 * Regex works for CodeWars tests, has problems with strings such as "truex", can be fixed by only allowing operators such as '(' and
+	 * ')' to be attached to booleans. Likewise with integers becoming identifiers if letters are attached. Easily fixed, but makes
+	 * getToken() a bit messier as regex pattern groups will change - Not important for testing against Simplexer class.
+	 */
 	
-	/* Lexer using regex */
-	
+	private static final Pattern WS_PATTERN = Pattern.compile("(\\s+)(.*)");
+	private static final Pattern INT_PATTERN = Pattern.compile("(\\d+)(.*)");
+	private static final Pattern BOOL_PATTERN = Pattern.compile("(true|false)(.*)");
+	private static final Pattern STR_PATTERN = Pattern.compile("(\"(\\w)*\")(.*)");
+	private static final Pattern OP_PATTERN = Pattern.compile("(\\+|\\-|\\*|\\/|\\%|\\(|\\)|\\=)(.*)");
+	private static final Pattern KW_PATTERN = Pattern.compile("(if|else|for|while|return|func|break)(.*)");
+	private static final Pattern ID_PATTERN = Pattern.compile("((\\w|\\$|\\_)+)((\\s(.*))*)");
+	private Matcher m;
+	private String buffer;
+
 	public RegexSimplexer(String buffer) {
-		
+		this.buffer = buffer;
+	}
+
+	public boolean hasNext() {
+		return buffer != null && buffer.length() > 0;
+	}
+
+	public Token next() {
+		if (!this.hasNext())
+			throw new NoSuchElementException();
+
+		if (isToken(WS_PATTERN))
+			return getToken("whitespace");
+		if (isToken(INT_PATTERN))
+			return getToken("integer");
+		if (isToken(BOOL_PATTERN))
+			return getToken("boolean");
+		if (isToken(STR_PATTERN))
+			return getToken("string");
+		if (isToken(OP_PATTERN))
+			return getToken("operator");
+		if (isToken(KW_PATTERN))
+			return getToken("keyword");
+		if (isToken(ID_PATTERN))
+			return getToken("identifier");
+
+		return new Token(null, null);
+	}
+
+	private boolean isToken(Pattern p) {
+		m = p.matcher(buffer);
+		return m.matches();
+	}
+
+	private Token getToken(String type) {
+		buffer = (type.equals("identifier") || type.equals("string")) ? m.group(3) : m.group(2);
+		return new Token(m.group(1), type);
 	}
 
 }
